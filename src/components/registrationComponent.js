d25ed6ece4cb8510ecb9fe48fbc0dd2c502fd444
joonasjouttijarvi <joonas.jouttijarvi@gmail.com>
2023-09-01 13:33:27 +0300

    database connection refactorin login and registrationcomponent prisma.schema to match mongodb

--- src/components/registrationComponent.js
+++ src/components/registrationComponent.js
@@ -1,7 +1,8 @@
-import { set, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
+import styles from "../styles/RegistrationForm.module.scss";
 
 function RegistrationForm() {
     const {
@@ -9,94 +10,53 @@ function RegistrationForm() {
         handleSubmit,
         formState: { errors },
     } = useForm();
-
     const [loading, setLoading] = useState(false);
-    const [success, setSuccess] = useState(false);
-    const [error, setError] = useState(false);
+    const [apiFeedback, setApiFeedback] = useState(null);
     const router = useRouter();
 
     const onSubmit = async (data) => {
         try {
+            setApiFeedback(null);
             setLoading(true);
             const response = await axios.post("/api/createUser", data);
             if (response.status === 200) {
-                setSuccess("User created successfully");
+                setApiFeedback({ type: 'success', message: "User created successfully" });
                 setTimeout(() => {
-                    router.push("/dashboard");
+                    router.push("/Login");
                 }, 2000);
-            } else {
-                setError("Something went wrong");
             }
         } catch (error) {
-            setError(error.response.data.message);
-        }
-        finally {
+            setApiFeedback({ type: 'error', message: error.response.data.message });
+        } finally {
             setLoading(false);
         }
     };
 
-
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label>Username</label>
-                <input
-                    type="text"
-                    id="username"
-                    {...register("username", { required: "Username is required" })}
-                />
-                {errors.username && <p>{errors.username.message}</p>}
-            </div>
-
-            <div>
-                <label>Email</label>
-                <input
-                    type="email"
-                    id="email"
-                    {...register("email", {
-                        required: "Email is required",
-                        pattern: {
-                            value: /\S+@\S+\.\S+/,
-                            message: "Invalid email address",
-                        },
-                    })}
-                />
-                {errors.email && <p>{errors.email.message}</p>}
-            </div>
-            <div>
-                <label>First Name</label>
-                <input
-                    type="text"
-                    id="firstName"
-                    {...register("firstName", { required: "First Name is required" })}
-                />
-                {errors.firstName && <p>{errors.firstName.message}</p>}
-                <div>
-                    <label>Last Name</label>
-                    <input
-                        type="text"
-                        id="lastName"
-                        {...register("lastName", { required: "Last Name is required" })}
-                    />
-                    {errors.lastName && <p>{errors.lastName.message}</p>}
+            {[
+                { label: 'Username', type: 'text', id: 'username', validation: { required: "Username is required" } },
+                { label: 'Email', type: 'email', id: 'email', validation: { required: "Email is required", pattern: { value: /\S+@\S+\.\S+/, message: "Invalid email address" } } },
+                { label: 'First Name', type: 'text', id: 'firstName', validation: { required: "First Name is required" } },
+                { label: 'Last Name', type: 'text', id: 'lastName', validation: { required: "Last Name is required" } },
+                { label: 'Password', type: 'password', id: 'password', validation: { required: "Password is required" } }
+            ].map(field => (
+                <div key={field.id}>
+                    <label>{field.label}</label>
+                    <input type={field.type} id={field.id} {...register(field.id, field.validation)} />
+                    {errors[field.id] && <p className={styles.inputError}>{errors[field.id].message}</p>}
                 </div>
-            </div>
-
-            <div>
-                <label>Password</label>
-                <input
-                    type="password"
-                    id="password"
-                    {...register("password", { required: "Password is required" })}
-                />
-                {errors.password && <p>{errors.password.message}</p>}
-            </div>
+            ))}
 
             <button type="submit" disabled={loading}>
                 {loading ? "Loading..." : "Register"}
             </button>
-            {success && <p>{styles.successMessage}{success}</p>}
-            {error && <p>{styles.errorMessage}{error}</p>}
+
+            {apiFeedback &&
+                <p className={apiFeedback.type === 'success' ? styles.successMessage : styles.errorMessage}>
+                    {apiFeedback.message}
+                </p>
+            }
         </form>
     );
 }
