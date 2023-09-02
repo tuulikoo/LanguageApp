a798e71c8efbb24ebfadb294c5a0ced71fa6862a
Irina Romjalis <iromjalis@gmail.com>
2023-09-02 16:29:14 +0300

    levels component

---
+++ src/pages/Levels.js
@@ -0,0 +1,32 @@
+import styles from "@/styles/Levels.module.css";
+
+const Levels = () => {
+    return <div className={styles.levels_container}>
+        <h1>🄻🄴🅅🄴🄻🅂 🅆🄴 🄷🄰🅅🄴</h1>
+        <ul className={styles.levels_list}>
+            <li className={styles.levels_item}>
+                <p>Tab 1</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
+                    autem ea facilis fugiat laboriosam.
+                </p>
+            </li>
+            <li className={styles.levels_item}>
+                <p>Tab 2</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
+                    autem ea facilis fugiat laboriosam.
+                </p>
+            </li>
+            <li className={styles.levels_item}>
+                <p>Tab 3</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
+                    autem ea facilis fugiat laboriosam.
+                </p>
+            </li>
+        </ul>
+    </div>
+}
+
+export default Levels;
