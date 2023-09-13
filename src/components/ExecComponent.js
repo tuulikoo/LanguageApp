0bb2f4c364f46812623bee2d26ec9e5cdd7a9150
joonasjouttijarvi <joonas.jouttijarvi@gmail.com>
2023-09-13 19:23:06 +0300

    point api and implementation on hearing exercises

--- src/components/ExecComponent.js
+++ src/components/ExecComponent.js
@@ -1,16 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { convertTextToSpeech } from '../utils/mimicApi';
 import styles from '../styles/Exec.module.scss';
+import { useUser } from '../utils/userContext';
+import wordList from '../utils/wordlists/wordList.json';
 
-const ExerciseComponent = ({ wordList }) => {
+const ExerciseComponent = () => {
+    const { user } = useUser();
+    const initialUserPoints = user ? user.userPoints : 0;
+    const userPoints = user ? user.userPoints : 0;
     const [inputWord, setInputWord] = useState('');
     const [audioURL, setAudioURL] = useState(null);
     const [result, setResult] = useState(null);
     const [currentIndex, setCurrentIndex] = useState(0);
 
+    const currentWordListKey = getWordListForPoints(userPoints);
+    const currentWordList = wordList[currentWordListKey] || [];
+    const [currentUserPoints, setCurrentUserPoints] = useState(initialUserPoints);
+
+
+
+
     const playAudio = async () => {
         try {
-            const audioBlob = await convertTextToSpeech(wordList[currentIndex]);
+            const audioBlob = await convertTextToSpeech(currentWordList[currentIndex]);
             const objectURL = URL.createObjectURL(audioBlob);
             setAudioURL(objectURL);
             new Audio(objectURL).play();
@@ -19,46 +31,89 @@ const ExerciseComponent = ({ wordList }) => {
         }
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        if (inputWord.trim().toLowerCase() === wordList[currentIndex].toLowerCase()) {
+
+        if (inputWord.trim().toLowerCase() === currentWordList[currentIndex].toLowerCase()) {
             setResult('Oikein!');
-            setCurrentIndex(prevIndex + 1);
+            setCurrentIndex(prevIndex => prevIndex + 1);
+            updateUserPoints();
         } else {
             setResult('väärin, yritä uudelleen!');
         }
+
         setInputWord('');
     };
 
-    const handleNext = () => {
-        if (currentIndex < wordList.length - 1) {
-            setCurrentIndex(prevIndex => prevIndex + 1);
-            setInputWord('');
-            setResult(null);
+    const updateUserPoints = async () => {
+        const newPoints = currentUserPoints + 1;
+        try {
+            const response = await fetch('/api/updatePoints', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ userId: user.id, newPoints })
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                setCurrentUserPoints(data.updatedPoints);  // Use the updater function here
+            } else {
+                throw new Error('Failed to update points');
+            }
+        } catch (error) {
+            console.error('Error:', error.message);
         }
     };
 
     return (
         <div className={styles.container}>
-            <button className={styles.audioButton} onClick={playAudio}>
-                <img src="images/audio.png" alt="Play Audio" />
-            </button>
-            <form onSubmit={handleSubmit}>
-                <input
-                    className={styles.input}
-                    value={inputWord}
-                    onChange={(e) => setInputWord(e.target.value)}
-                    placeholder="Kirjoita kuulemasi sana tähän"
-                />
-                <button className={styles.submitButton} type="submit"></button>
-            </form>
-            {result && (
-                <p className={`${styles.result} ${result === 'Oikein!' ? styles.correct : ''}`}>{result}</p>
-            )}
-            <button onClick={handleNext}>Seuraava</button>
+            <AudioButton onPlay={playAudio} />
+            <ExerciseForm inputWord={inputWord} onInputChange={setInputWord} onSubmit={handleSubmit} />
+            <ResultDisplay result={result} />
+            <NextButton currentIndex={currentIndex} wordListLength={currentWordList.length} onNext={() => setCurrentIndex(prevIndex => prevIndex + 1)} />
         </div>
     );
 };
 
+const AudioButton = ({ onPlay }) => (
+    <button className={styles.audioButton} onClick={onPlay}>
+        <img src="images/audio.png" alt="Play Audio" />
+    </button>
+);
+
+const ExerciseForm = ({ inputWord, onInputChange, onSubmit }) => (
+    <form onSubmit={onSubmit}>
+        <input
+            className={styles.input}
+            value={inputWord}
+            onChange={(e) => onInputChange(e.target.value)}
+            placeholder="Kirjoita kuulemasi sana tähän"
+        />
+        <button className={styles.submitButton} type="submit"></button>
+    </form>
+);
+
+const ResultDisplay = ({ result }) => (
+    result && (
+        <p className={`${styles.result} ${result === 'Oikein!' ? styles.correct : ''}`}>{result}</p>
+    )
+);
+
+const NextButton = ({ currentIndex, wordListLength, onNext }) => (
+    currentIndex < wordListLength - 1 && (
+        <button onClick={onNext}>Seuraava</button>
+    )
+);
+
+function getWordListForPoints(points) {
+    if (points < 19) return "listening1.1";
+    if (points > 40) return "listening1.2";
+    return "listening2.0";
+}
+
 export default ExerciseComponent;
+
+
+
+
 
--- src/pages/Exec1.js
+++ src/pages/Exec1.js
@@ -1,12 +1,10 @@
 import ExerciseComponent from "@/components/ExecComponent";
-import wordLists from '../utils/wordlists/TextTospeech.json';
 
 export default function Exec1() {
-    const selectedList = wordLists["listening1.8"]; // for Testing TODO: change acconding to user level 
 
     return (
         <div>
-            <ExerciseComponent wordList={selectedList} />
+            <ExerciseComponent />
         </div>
     );
 }
