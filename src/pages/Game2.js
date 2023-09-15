eef69e60b54983268bf04cad35ec77dd96297c36
joonasjouttijarvi <joonas.jouttijarvi@gmail.com>
2023-09-15 23:25:15 +0300

    fiddling with points in speech exercise

---
+++ src/pages/Game2.js
@@ -0,0 +1,11 @@
+import ExerciseComponent from "@/components/Game2Component";
+
+export default function Game2() {
+
+    return (
+        <div>
+            <ExerciseComponent />
+        </div>
+    );
+}
+
