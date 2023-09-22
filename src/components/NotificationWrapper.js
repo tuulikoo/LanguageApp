2ab8916bd854a5249e770620b6c1b36a57ed2cda
joonasjouttijarvi <joonas.jouttijarvi@gmail.com>
2023-09-22 21:50:11 +0300

    Toastings

--- src/components/NotificationWrapper.js
+++ src/components/NotificationWrapper.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect, useContext, createContext } from "react";
 import { useUser } from "../utils/userContext";
 import styles from "../styles/NotificationWrapper.module.scss";
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import BookIcon from '@mui/icons-material/Book';
+import StarIcon from '@mui/icons-material/Star';
+import CoffeeIcon from '@mui/icons-material/LocalCafe';
+
 
 const NotificationContext = createContext();
 
@@ -12,6 +17,7 @@ const NotificationWrapper = ({ children }) => {
     const [isOpen, setIsOpen] = useState(false);
     const [message, setMessage] = useState("");
     const { user } = useUser();
+    const [icon, setIcon] = useState();
 
     useEffect(() => {
         //wait for user to be loaded
@@ -21,15 +27,20 @@ const NotificationWrapper = ({ children }) => {
 
         if (timeSpent === 0) {
             setMessage("Aloita englannin kielen opsiskelu!");
+            setIcon(<BookIcon />);
         } else if (timeSpent > 0 && timeSpent <= 600) {
             setMessage(`Olet treenannut ${totalTime} minuuttia! Jatka samaan malliin`);
+            setIcon(<StarIcon />);
         } else if (timeSpent > 600 && timeSpent <= 1200) {
             setMessage(`Loistavaa! Olet harjoitellut jo ${totalTime} minuuttia. Jatka samaan malliin!`);
+            setIcon(<CoffeeIcon />);
         } else {
             setMessage(`Oho! olet harjoitellut jo ${totalTime} minuuttia. Jatka samaan malliin!`);
+            setIcon(<ThumbUpIcon />);
         }
 
         setIsOpen(true);
+
 
         const timeoutId = setTimeout(() => {
             setIsOpen(false);
@@ -50,6 +61,7 @@ const NotificationWrapper = ({ children }) => {
             {children}
             {isOpen && (
                 <div className={`${styles.toast} ${isOpen ? styles.show : ""}`}>
+                    <div className={styles.icon}>{icon}</div>
                     <button className={styles.close} onClick={() => setIsOpen(false)}>
                         &times;
                     </button>
