da1aa8c425c9f01eef19e054f432af2105f24a6f
joonasjouttijarvi <joonas.jouttijarvi@gmail.com>
2023-09-25 18:26:36 +0300

    switched mimic3 server to ecloud

--- src/utils/mimicApi.js
+++ src/utils/mimicApi.js
@@ -1,21 +1,21 @@
-const MIMIC_SERVER_URL = 'http://localhost:59125/api/tts';
-
-const DEFAULT_VOICE = 'uk/apope_low';
+const DEFAULT_VOICE = 'en_US/ljspeech_low';
+const MIMIC3_SERVER = process.env.NEXT_PUBLIC_MIMIC3_SERVER;
 
 export const convertTextToSpeech = async (text, options = {}) => {
     const params = new URLSearchParams({
         text,
-        voice: 'en_US/ljspeech' || DEFAULT_VOICE,
+        voice: DEFAULT_VOICE,
         noiseScale: options.noiseScale || 0.222,
         noiseW: options.noiseW || 0.3,
         lengthScale: options.lengthScale || 1.5,
-        ssml: options.ssml || true,
+        ssml: options.ssml || false,
     });
 
-    const response = await fetch(`${MIMIC_SERVER_URL}?${params.toString()}`);
+    const response = await fetch(MIMIC3_SERVER + params.toString());
     if (!response.ok) {
         throw new Error('Failed to convert text to speech');
     }
 
     return await response.blob();  // blob for audio data
 };
+
