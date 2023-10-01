afc6eefc7d7bd49dbb678aa8f5fd844b23657b5e
joonasjouttijarvi <joonas.jouttijarvi@gmail.com>
2023-10-01 21:07:25 +0300

    added test for jwt token

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,14 +2,15 @@ import jwt from "jsonwebtoken";
 
 const KEY = process.env.JWT_SECRET;
 
-export const generateToken = (payload) => {
-    return jwt.sign(payload, KEY, { expiresIn: "1h" });
+export const generateToken = (payload, key = KEY) => {
+    return jwt.sign(payload, key, { expiresIn: "1h" });
 };
 
-export const verifyToken = (token) => {
+export const verifyToken = (token, key = KEY) => {
     try {
-        return jwt.verify(token, KEY);
+        return jwt.verify(token, key);
     } catch (error) {
         return null;
     }
 };
+
--- test/Jwt.test.js
+++ test/Jwt.test.js
@@ -1,15 +1,26 @@
-import { verifyToken } from '../src/utils/jwt.js';
+import { generateToken, verifyToken } from '../src/utils/jwt';
+import jwt from 'jsonwebtoken';
+import { describe, it, expect } from 'vitest';
 
-describe('JWT utilities', () => {
+describe('JWT utility functions', () => {
+    const mockSecret = 'my-test-secret';
 
-    beforeAll(() => {
-        process.env.JWT_SECRET = 'testSecret';
+    it('should generate a token and verify it', async () => {
+        const payload = { userId: '12345', role: 'user' };
+
+        const token = generateToken(payload, mockSecret);
+        expect(token).toBeTruthy();
+
+        const decodedPayload = verifyToken(token, mockSecret);
+        expect(decodedPayload.userId).toBe(payload.userId);
+        expect(decodedPayload.role).toBe(payload.role);
     });
 
-
-    it('should return null for an invalid token', () => {
-        const decodedUser = verifyToken('invalidToken');
-        expect(decodedUser).toBeNull();
+    it('should return null when verifying invalid token', async () => {
+        const invalidToken = jwt.sign({ foo: 'bar' }, 'wrong-secret', { expiresIn: '1h' });
+        const result = verifyToken(invalidToken, mockSecret);
+        expect(result).toBeNull();
     });
+
 });
 
