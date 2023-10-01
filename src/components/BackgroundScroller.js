2c3cf5556a886813f32357f3dc79033f547a1f40
joonasjouttijarvi <joonas.jouttijarvi@gmail.com>
2023-10-01 14:15:20 +0300

    someadiitions

--- src/components/BackgroundScroller.js
+++ src/components/BackgroundScroller.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
-import styles from '../styles/BackgroundScroller.module.scss';
+
 const svgList = [
+
     'undraw_bibliophile_re_xarc.svg',
     'undraw_reading_re_29f8.svg',
     'undraw_ride_a_bicycle_re_6tjy.svg',
 ];
+
 const BackgroundScroller = () => {
     const [scrollY, setScrollY] = useState(0);
 
@@ -23,9 +25,8 @@ const BackgroundScroller = () => {
     }, []);
 
     return (
-        <div style={{ position: "absolute", top: 0, left: 10, width: '100%', height: '90%' }}>
+        <div style={{ position: "absolute", top: 0, left: 0, width: '100%', height: '100%', overflow: 'hidden' }}>
             {svgList.map((svg, index) => {
-
                 const baseOffset = index * 100;
 
                 return (
@@ -35,10 +36,11 @@ const BackgroundScroller = () => {
                             position: 'absolute',
                             top: `${baseOffset - scrollY * (index + 1)}vh`,
                             [index % 2 === 0 ? 'left' : 'right']: 0,
-                            width: '50vw',
-                            height: '100vh',
+                            width: '50%',
+                            height: '100%',
                             backgroundImage: `url('/svg/${svg}')`,
-                            backgroundSize: 'cover',
+                            backgroundSize: 'contain',
+                            backgroundRepeat: 'no-repeat',
                             backgroundPosition: 'center',
                             transition: 'background 0.3s ease-in-out',
                             zIndex: -1
