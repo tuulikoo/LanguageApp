62d93f5e2eccfe8751fcb5ec20b79f637417d7ef
joonasjouttijarvi <joonas.jouttijarvi@gmail.com>
2023-10-07 12:52:49 +0300

    added level1 stuff

--- src/components/FlashcardComponent.js
+++ src/components/FlashcardComponent.js
@@ -1,29 +1,26 @@
-import React, { useState } from "react";
-import styles from "../styles/flashcards.module.scss";
+import React, { useState } from 'react';
+import styles from '../styles/flashcards.module.css'; // Import the CSS module
 
 const Flashcard = ({ word, definition, onNext }) => {
-  const [isFlipped, setIsFlipped] = useState(false);
+    const [isFlipped, setIsFlipped] = useState(false);
 
-  const handleFlip = () => {
-    setIsFlipped(!isFlipped);
-  };
+    const handleFlip = () => {
+        setIsFlipped(!isFlipped);
+    };
 
-  return (
-    <div className={styles.flashcard}>
-    <div className={`${styles.card} ${isFlipped ? styles.flipped : ''}`} onClick={handleFlip}>
-        <div className={styles.front}>
-            <h3>{word}</h3>
+    return (
+        <div className={styles.flashcard}>
+            <div
+                className={`${styles.card} ${isFlipped ? styles.flipped : ''}`} // Use CSS module class names
+                onClick={handleFlip}
+            >
+                <h3>{isFlipped ? definition : word}</h3>
+            </div>
+            <div className={styles.arrow} onClick={() => { setIsFlipped(false); onNext(); }}>
+                →
+            </div>
         </div>
-        <div className={styles.back}>
-            <h3>{definition}</h3>
-        </div>
-    </div>
-    <div className={styles.arrow} onClick={() => { setIsFlipped(false); onNext(); }}>
-        →
-    </div>
-</div>
-
-  );
+    );
 };
 
 export default Flashcard;
--- src/components/FlashcardDeckComponent.js
+++ src/components/FlashcardDeckComponent.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Flashcard from './FlashcardComponent';
-import styles from '../styles/flashcards.module.scss';
+import styles from '../styles/flashcards.module.css';
+import { textToSpeech } from "@/utils/mimicApi"; // Import the CSS module
 
 const FlashcardDeckComponent = ({ flashcards }) => {
     const [currentCard, setCurrentCard] = useState(0);
@@ -9,13 +10,31 @@ const FlashcardDeckComponent = ({ flashcards }) => {
         setCurrentCard((prevCard) => (prevCard + 1) % flashcards.length);
     };
 
+    const handleSpeakButtonClick = () => {
+        textToSpeech(flashcards[currentCard].word);
+    };
+
     return (
         <div className={styles.flashcardDeck}>
             <Flashcard
+
                 word={flashcards[currentCard].word}
                 definition={flashcards[currentCard].definition}
                 onNext={handleNext}
             />
+
+            <div className={styles.centeredContainer}>
+                <div className={styles.speakButton}>
+                    <h2> Kuuntele </h2>
+                    <button onClick={handleSpeakButtonClick}>
+                        <img
+                            src='/images/audio.png'
+                            alt="Speaker Button"
+                        />
+                    </button>
+                </div>
+            </div>
+
         </div>
     );
 };
