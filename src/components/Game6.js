f9ac1cafcb31167bfd541a3926ba178b2129a0c1
joonasjouttijarvi <joonas.jouttijarvi@gmail.com>
2023-10-10 13:42:56 +0300

    jenkinsfile

--- src/components/Game6.js
+++ src/components/Game6.js
@@ -1,10 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Typography, RadioGroup, FormControlLabel, Radio, Container } from '@mui/material';
-import stories from '../utils/wordlists/stories.json';
-import { textToSpeech } from '../utils/mimicApi';
-import styles from '../styles/Game6Component.module.scss';
-import updateUserPoints from '../helpers/PointAdder';
-import {useUser} from '../utils/userContext';
+import React, { useState, useEffect } from "react";
+import {
+    Button,
+    Typography,
+    RadioGroup,
+    FormControlLabel,
+    Radio,
+    Container,
+} from "@mui/material";
+import stories from "../utils/wordlists/stories.json";
+import { textToSpeech } from "../utils/mimicApi";
+import styles from "../styles/Game6Component.module.scss";
+import updateUserPoints from "../helpers/PointAdder";
+import { useUser } from "../utils/userContext";
 
 function GameStoryComponent() {
     const [currentStoryIndex, setCurrentStoryIndex] = useState(0);
@@ -12,7 +19,7 @@ function GameStoryComponent() {
     const [selectedAnswer, setSelectedAnswer] = useState(null);
     const [feedback, setFeedback] = useState("");
     const [isStoryStarted, setIsStoryStarted] = useState(false);
-    const {user} = useUser();
+    const { user } = useUser();
 
     const loadNewStory = () => {
         setCurrentStoryIndex((prevIndex) => (prevIndex + 1) % stories.length);
@@ -44,54 +51,80 @@ function GameStoryComponent() {
         });
     };
 
-   
-
-
     const handleSubmit = () => {
         if (selectedAnswer == stories[currentStoryIndex].correctAnswer) {
             setFeedback("Oikein");
-            updateUserPoints(user.id , 2);
+            updateUserPoints(user.id, 2);
         } else {
             setFeedback("Väärin, yritä uudelleen");
         }
     };
 
- return (
-    <Container className={styles.container}>
-        <Typography variant="h5" gutterBottom>Kuuntele lyhyt tarina ja valitse vastaus tarinan mukaan</Typography>
-        {!isStoryStarted ? (
-            <div className={styles.startButton}>
-                <Button onClick={startStory} variant="contained" className={styles.primaryButton}>Aloita Tarina</Button>
-            </div>
-        ) : (
-            <>
-                <div className={styles.storyContent}>
-                    <Typography variant="body1" gutterBottom>
-                        {displayedText}
-                    </Typography>
-                    {stories[currentStoryIndex] && (
-                        <>
-                            <RadioGroup value={selectedAnswer} onChange={(e) => setSelectedAnswer(e.target.value)}>
-                                {stories[currentStoryIndex].options.map((option, index) => (
-                                    <FormControlLabel
-                                        key={index}
-                                        value={`${index}`}
-                                        control={<Radio />}
-                                        label={option}
-                                    />
-                                ))}
-                            </RadioGroup>
-                            <Typography color="primary" gutterBottom className={styles.feedback}>{feedback}</Typography>
-                            <Button onClick={handleSubmit} variant="contained" className={styles.primaryButton}>Vastaa</Button>
-                            <Button onClick={loadNewStory} variant="contained" style={{ marginLeft: '10px' }} className={styles.primaryButton}>Seuraava tarina</Button>
-                        </>
-                    )}
+    return (
+        <Container className={styles.container}>
+            <Typography variant="h5" gutterBottom>
+                Kuuntele lyhyt tarina ja valitse vastaus tarinan mukaan
+            </Typography>
+            {!isStoryStarted ? (
+                <div className={styles.startButton}>
+                    <Button
+                        onClick={startStory}
+                        variant="contained"
+                        className={styles.primaryButton}
+                    >
+                        Aloita Tarina
+                    </Button>
                 </div>
-            </>
-        )}
-    </Container>
-);
+            ) : (
+                <>
+                    <div className={styles.storyContent}>
+                        <Typography variant="body1" gutterBottom>
+                            {displayedText}
+                        </Typography>
+                        {stories[currentStoryIndex] && (
+                            <>
+                                <RadioGroup
+                                    value={selectedAnswer}
+                                    onChange={(e) => setSelectedAnswer(e.target.value)}
+                                >
+                                    {stories[currentStoryIndex].options.map((option, index) => (
+                                        <FormControlLabel
+                                            key={index}
+                                            value={`${index}`}
+                                            control={<Radio />}
+                                            label={option}
+                                        />
+                                    ))}
+                                </RadioGroup>
+                                <Typography
+                                    color="primary"
+                                    gutterBottom
+                                    className={styles.feedback}
+                                >
+                                    {feedback}
+                                </Typography>
+                                <Button
+                                    onClick={handleSubmit}
+                                    variant="contained"
+                                    className={styles.primaryButton}
+                                >
+                                    Vastaa
+                                </Button>
+                                <Button
+                                    onClick={loadNewStory}
+                                    variant="contained"
+                                    style={{ marginLeft: "10px" }}
+                                    className={styles.primaryButton}
+                                >
+                                    Seuraava tarina
+                                </Button>
+                            </>
+                        )}
+                    </div>
+                </>
+            )}
+        </Container>
+    );
 }
 
 export default GameStoryComponent;
-
