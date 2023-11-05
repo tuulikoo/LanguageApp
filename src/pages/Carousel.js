b4adf6b62630d3448b7e482983fe0ad009ac2235
joonasjouttijarvi <joonas.jouttijarvi@gmail.com>
2023-11-05 20:31:51 +0200

    added old carousel

--- src/pages/Carousel.js
+++ src/pages/Carousel.js
@@ -1,29 +1,31 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import items from "../utils/wordlists/carousel.json";
 import styles from "../styles/Carousel.module.scss";
 
+
 export default function ResponsiveCarousel() {
     return (
         <div className={styles.carousel_container}>
+            <h1 className={styles.carousel_title}></h1>
             <div className={styles.container}>
                 <Carousel
-                    showArrows={true}
+                    showArrows={false}
                     autoPlay={true}
-                    showIndicators={true}
+                    showIndicators={false}
                     infiniteLoop={true}
                     dynamicHeight={false}
+                    showStatus={false}
+                    showThumbs={false}
+                    interval={10000}
                     className={styles.mySwiper}
                     swipeable={true}
                 >
-                    {items.map(({ id, imageUrl, title, text }) => (
+                    {items.map(({ id, quote_finnish, quote_english }) => (
                         <div key={id} className={styles.swipItem}>
-                            <div className={styles.imgBox}>
-                                <img src={imageUrl} alt="slides" />
-                            </div>
                             <div className={styles.detail}>
-                                <h2 className="font-custom">{title}</h2>
-                                <p className="font-custom">{text}</p>
+                                <h2>{quote_finnish}</h2>
+                                <p>{quote_english}</p>
                             </div>
                         </div>
                     ))}
