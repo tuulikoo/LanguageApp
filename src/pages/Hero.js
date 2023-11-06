6dd0466afa68031ebdb06588cd65a847d4eea8dc
joonasjouttijarvi <joonas.jouttijarvi@gmail.com>
2023-11-06 23:00:11 +0200

    frontpage levels localization

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,6 +1,5 @@
 import styles from '../styles/Hero.module.css';
 import { useTranslation } from 'react-i18next';
-//spinner from mui
 import CircularProgress from '@mui/material/CircularProgress';
 import { useState, useEffect } from 'react';
 
@@ -9,33 +8,28 @@ import { useState, useEffect } from 'react';
 const Hero = () => {
 
     const { t } = useTranslation();
-    const [isloading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        setIsLoading(false);
-    }, []);
 
-    if (isloading) {
-        return (
-            <div className={styles.spinner}>
-                <CircularProgress />
-            </div>
-        );
-    }
-
-    return (
+ return (
         <div className={styles.hero_container}>
             <div className={styles.content_overlay}>
                 <h1 className={styles.hero_title}>
-                    <span className={styles.primary_text}>Aloita</span><br />
-                    <span className={styles.secondary_text}>Kielen Opiskelu</span>
+                    <span className={styles.primary_text}>{t('startLearning')}</span><br />
+                    <span className={styles.secondary_text}>{t('languageStudy')}</span>
                 </h1>
-                <a href="Levels" className={styles.hero_button}>
+                <a href="/Levels" className={styles.hero_button}>
                     {t('heroButton')}
                 </a>
             </div>
         </div>
     );
-}
+};
+
 export default Hero;
+
+
+
+
+
+
 
