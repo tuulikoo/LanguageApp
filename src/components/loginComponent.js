7d80226ca86769ecc88c5d587d3a9fbaf04431b8
joonasjouttijarvi <joonas.jouttijarvi@gmail.com>
2023-11-09 23:27:04 +0200

    Change dockerfile branch to master

--- src/components/loginComponent.js
+++ src/components/loginComponent.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import styles from '../styles/Login.module.scss';
+import styles from '../styles/login.module.scss';
 import { useUser } from '../utils/userContext';
 
 const MAX_LOGIN_ATTEMPTS = 5;
