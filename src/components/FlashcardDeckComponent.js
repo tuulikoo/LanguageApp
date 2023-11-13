2148adcec152276aae7b38f305d3f8127c8308aa
tuulikoo <tuuli.kivisaari@gmail.com>
2023-11-13 10:40:32 +0200

    Flashcarddeck lokaisointi

--- src/components/FlashcardDeckComponent.js
+++ src/components/FlashcardDeckComponent.js
@@ -2,15 +2,19 @@ import React, { useState } from "react";
 import styles from "../styles/flashcards.module.css";
 import { textToSpeech } from "@/utils/mimicApi";
 import { Image } from "react-bootstrap";
+import { useTranslation } from 'react-i18next';
+import {t} from "i18next";
 
-const Flashcard = ({ word, definition, isFlipped, onFlip, onNext }) => {
+const Flashcard = ({ word, definition, isFlipped, onFlip, onNext, lang }) => {
+    const { t } = useTranslation();
+
     return (
         <div className={styles.flashcard}>
             <div
                 className={`${styles.card} ${isFlipped ? styles.flipped : ""}`}
                 onClick={onFlip}
             >
-                <h3>{isFlipped ? definition : word}</h3>
+                <h3>{isFlipped ? t(definition[lang]) : word}</h3>
             </div>
             <div className={styles.arrow} onClick={onNext}>
                 →
@@ -22,13 +26,14 @@ const Flashcard = ({ word, definition, isFlipped, onFlip, onNext }) => {
 const FlashcardDeck = ({ flashcards }) => {
     const [currentCard, setCurrentCard] = useState(0);
     const [isFlipped, setIsFlipped] = useState(false);
+    const { i18n } = useTranslation();
 
     const handleFlip = () => {
         setIsFlipped(!isFlipped);
     };
 
     const handleNext = () => {
-        setIsFlipped(false); // Reset flip when moving to next card
+        setIsFlipped(false); // Reset flip when moving to the next card
         setCurrentCard((prevCard) => (prevCard + 1) % flashcards.length);
     };
 
@@ -40,15 +45,16 @@ const FlashcardDeck = ({ flashcards }) => {
         <div className={styles.flashcardDeck}>
             <Flashcard
                 word={flashcards[currentCard].word}
-                definition={flashcards[currentCard].definition}
+                definition={flashcards[currentCard].definitions}
                 isFlipped={isFlipped}
                 onFlip={handleFlip}
                 onNext={handleNext}
+                lang={i18n.language}
             />
 
             <div className={styles.centeredContainer}>
                 <div className={styles.speakButton}>
-                    <h2> Kuuntele </h2>
+                    <h2> {t("G4listen")} </h2>
                     <button onClick={handleSpeakButtonClick}>
                         <Image src="/images/audio.png" alt="Speaker Button" />
                     </button>
