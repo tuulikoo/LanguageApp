8b16c67bcef89d50525d590fb0ae758ec0eb9c06
Joonas Jouttijärvi <joonas.jouttijarvi@metropolia.fi>
2023-12-04 23:05:21 +0200

    refac

--- src/helpers/PointAdder.js
+++ src/helpers/PointAdder.js
@@ -6,23 +6,23 @@
  * @returns {Promise<number>} - The updated point total after adding.
  */
 async function updateUserPoints(userId, pointsToAdd) {
-  try {
-    const response = await fetch("/api/updatePoints", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ userId, newPoints: pointsToAdd }),
-    });
+    try {
+        const response = await fetch("/api/updatePoints", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ userId, newPoints: pointsToAdd }),
+        });
 
-    if (response.ok) {
-      const data = await response.json();
-      return data.updatedPoints;
-    } else {
-      throw new Error("Failed to update points");
+        if (response.ok) {
+            const data = await response.json();
+            return data.updatedPoints;
+        } else {
+            throw new Error("Failed to update points");
+        }
+    } catch (error) {
+        console.error("Error:", error.message);
+        throw error;
     }
-  } catch (error) {
-    console.error("Error:", error.message);
-    throw error;
-  }
 }
 
 export default updateUserPoints;
